07fcc8445910aa21d8b2abd6680a75cfcd27e1aa
Ahmed Serifoski <ahmed.serifoski@gmail.com>
2024-01-08 21:54:09 +0100

    just css

--- views/layout/header.php
+++ views/layout/header.php
@@ -9,5 +9,5 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,500;0,600;1,200&family=Open+Sans:wght@300;400;500&family=Roboto+Flex:opsz,wght@8..144,100;8..144,300;8..144,500&family=Roboto+Mono:wght@100;200;300;400&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
 </head>
-<body class="bg-info">
+<body class="bg-info" style="font-family: 'Montserrat', sans-serif;">
  
--- views/todo/index.php
+++ views/todo/index.php
@@ -2,7 +2,7 @@
 
     <?php include __DIR__ . "/../layout/header.php"; ?>
     <div class="d-flex flex-column align-items-center">
-        <h1 class="text-light mt-5" style="font-family: 'Montserrat', sans-serif;" >My Todos</h1>
+        <h1 class="text-light mt-5" >My Todos</h1>
         <p class="text-light" style="font-family: 'Montserrat', sans-serif;font-weigt:200;font-style:italic;">once to criss, twice to cross</p>
         <!-- <ul class="list-group mt-3" style="min-width: 500px;">
             <?php foreach($todos as $todo):?>
@@ -25,7 +25,7 @@
         </ul>
         <form method="post" action="addTodo" id="add-todo-form" class="d-flex justify-content-between mt-3">
             <input type="text" class="form-control" id="todo" name="todo" placeholder="Enter a new todo">
-            <button type="submit" class="btn btn-primary" id="add-todo-btn">Add To-do</button>
+            <button type="submit" class="btn btn-warning" id="add-todo-btn">Add To-do</button>
         </form>
         
     </div>
