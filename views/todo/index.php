a4eae6581142d45e405ba0fe1392768c58f0817b
Ahmed Serifoski <ahmed.serifoski@gmail.com>
2024-01-09 00:05:47 +0100

    start criss cross todo

--- views/todo/index.php
+++ views/todo/index.php
@@ -4,11 +4,6 @@
     <div class="d-flex flex-column align-items-center">
         <h1 class="text-light mt-5" >My Todos</h1>
         <p class="text-light" style="font-family: 'Montserrat', sans-serif;font-weigt:200;font-style:italic;">once to criss, twice to cross</p>
-        <!-- <ul class="list-group mt-3" style="min-width: 500px;">
-            <?php foreach($todos as $todo):?>
-                <li class="list-group-item bg-warning"><?php echo($todo["todo"]); ?></li>
-            <?php endforeach; ?>
-        </ul> -->
         <ul class="list-group mt-3" style="min-width: 500px;">
             <?php foreach($todos as $todo):?>
                 <li class="list-group-item bg-warning" 
@@ -27,10 +22,7 @@
             <input type="text" class="form-control" id="todo" name="todo" placeholder="Enter a new todo">
             <button type="submit" class="btn btn-warning" id="add-todo-btn">Add To-do</button>
         </form>
-        
     </div>
-    <?php ?>
-
 <?php include __DIR__ . "/../layout/footer.php";?>
 
     
